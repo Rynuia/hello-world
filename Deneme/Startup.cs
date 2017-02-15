b009fc5d55332accf7566ae1d99029eb0697a5fa
MERT <MERT@DESKTOP-N417KK5>
2017-02-15 21:11:05 +0300

    Add project files.

---
+++ Deneme/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Deneme.Startup))]
+namespace Deneme
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
